1970d34a4471a01f9e52bf213c9042d4d09b6bf8
Bulhwi Cha <chabulhwi@semmalgil.com>
2025-03-27 10:20:31 +0900

    Add constructive proof of barber paradox

--- TPIL/Chapter04/Question03.lean
+++ TPIL/Chapter04/Question03.lean
@@ -1,6 +1,7 @@
 variable (men : Type) (barber : men)
 variable (shaves : men → men → Prop)
 
+/-- Bulhwi: this proof uses classical reasoning. -/
 example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False := by
   let contradiction := h barber
   let forward := contradiction.mp
@@ -8,3 +9,12 @@ example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False := by
   by_cases s : shaves barber barber
   · exact forward s s
   · exact s (backward s)
+
+/-- Bulhwi: a constructive proof of the barber paradox. -/
+example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False :=
+  have contradiction := h barber
+  have forward := contradiction.mp
+  have backward := contradiction.mpr
+  have ns : ¬shaves barber barber := fun s => forward s s
+  have s : shaves barber barber := backward ns
+  show False from ns s
