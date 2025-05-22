cc537dc55d3729c186ee5be2e8372be421f80bff
juyonLee00 <ljy012900@gmail.com>
2025-05-22 18:29:05 +0900

    Fix chapter05 questions

--- TPIL/Chapter05/Question01_04.lean
+++ TPIL/Chapter05/Question01_04.lean
@@ -2,8 +2,10 @@ variable {α : Type} (p q : α → Prop)
 open Classical
 
 -- 1.
-example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := by
-  simp -- simp made no progress
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
+Iff.intro
+  (fun h => ⟨fun x => (h x).left, fun x => (h x).right⟩)
+  (fun ⟨hp, hq⟩ => fun x => ⟨hp x, hq x⟩)
 
 example : (∀ x, p x → q x) → (∀ x, p x) → ∀ x, q x := by
   intro h₁ h₂ x
