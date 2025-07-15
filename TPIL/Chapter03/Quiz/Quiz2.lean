73cd212e74c26165a54397154ae8277c9f1d33a5
juyonLee00 <ljy012900@gmail.com>
2025-07-15 12:10:56 +0900

    Fix workflow error

--- TPIL/Chapter03/Quiz/Quiz2.lean
+++ TPIL/Chapter03/Quiz/Quiz2.lean
@@ -3,13 +3,13 @@
 -/
 
 /-(1)-/
-  theorem twoPlusThreeIsFive : 2 + 3 = 5 := by simp
+  theorem twoPlusThreeIsFiveTheorem : 2 + 3 = 5 := by simp
 
 /-(2)-/
-  theorem fifteenMinusEightIsSeven : 15 - 8 = 7 := by simp
+  theorem fifteenMinusEightIsSevenTheorem : 15 - 8 = 7 := by simp
 
 /-(3)-/
-  theorem helloAppendWorldIsHelloworld : "Hello, "++ "world" = "Hello, world" := by simp
+  theorem helloAppendWorldIsHelloworldTheorem : "Hello, "++ "world" = "Hello, world" := by simp
 
 /-(4)-/
-  theorem eighteenIsBiggerThanFive : 5 < 18 := by simp
+  theorem eighteenIsBiggerThanFiveTheorem : 5 < 18 := by simp
