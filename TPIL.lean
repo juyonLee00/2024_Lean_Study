2296a7d23844a68057f2645eca327b9fa2b40664
Bulhwi Cha <chabulhwi@semmalgil.com>
2026-02-16 16:04:29 +0900

    chore: reorder `import` statements

--- TPIL.lean
+++ TPIL.lean
@@ -55,5 +55,5 @@ import TPIL.Chapter05.Exercise02
 import TPIL.Chapter07.Injective
 import TPIL.Chapter07.«Question02-13»
 import TPIL.Chapter07.«Question15-16»
-import TPIL.Chapter08.StructuralRecursion
 import TPIL.Chapter07.Quiz07
+import TPIL.Chapter08.StructuralRecursion
